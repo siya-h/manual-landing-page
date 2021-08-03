0e6f64b0ecd7c15b66c7f7225126828f54b7705b
Siyabonga Hlengwa <siyahlengwa@Siyas-Macbook-Pro.local>
2021-08-03 19:11:38 +0200

    styling header and making it responsive

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -7,21 +7,37 @@ import logo from '../assets/images/logo.svg'
 const Heading = () => {
     return (
         <Box
-            bgSize="contain"
+            bgSize="100% 100%"
             bgRepeat="no-repeat"
-            w="100%"
+            w={['100%']}
+            h={['240px', '340px','460px' ,'680px']}
             bgImage={bgImg}
+            p={1}
         >
-            <Link to="/">
-                <Image
-                    src={logo}
-                />
-            </Link>
-            <Text transform="capitalize">BE GOOD TO YOURSELF</Text>
-            <Text>We’re working around the clock to bring you a holistic approach to your wellness. From top to bottom, inside and out.</Text>
-            <Link to="/quiz">
-                <Button as="button" bg="tealish" borderRadius={['49.5px']} w={[100, 150, 200]} color="white">TAKE THE QUIZ</Button>
-            </Link>
+            <Box
+                mt={['10px', '20px','25px' ,'30px']}
+            >
+                <Link to="/">
+                    <Image
+                        src={logo}
+                        w={['20px', '36px','40px' ,'46px']}
+                        h={['22px', '34px', '34px','42px']}
+                        ml={['14px','28px','38px','70px']}
+                    />
+                </Link>
+            </Box>
+
+            <Box fontSize={['22px', '36px','54px' ,'80px']} lineHeight={['22px', '42px', '62px','98px']} w={['55%', '55%', '60%','64%']} h={['15%', '20%','25%' ,'30%']} ml={['12px', '26px','34px', '60px']} mt={['15px', '25px','40px','90px']}>
+                <Text transform="capitalize" >BE GOOD</Text>
+                <Text transform="capitalize" >TO YOURSELF</Text>
+            </Box>
+                <Text fontFamily="Helvetica" fontSize={['12px', '14px','16px','18px']} lineHeight={['12px', '18px','24px','30px']} w={['55%', '40%','40%','400px']} h={['60px','65px','75px',]} ml={['14px','28px','38px','70px']} mt={['18px','26px',,]} >We’re working around the clock to bring you a holistic approach to your wellness. From top to bottom, inside and out.</Text>
+            <Box ml={['14px', '28px', '38px','70px']} mt={['4px', '20px', '35px','50px']} display="inline-block" >
+                <Link to="/quiz">
+                    <Button bg="tealish" w={['100px', '160px','180px' ,'200px']} h={['38px', '40px','44px' ,'48px']} boxShadow={['0px 10px 30px -20px rgba(0,0,0,0.5)']} borderRadius={['49.5px']} fontSize={['10px', '12px', '12px', '12px']} letterSpacing={['1px',,,'2.4px']} maxWidth="200px" minWidth="150px" fontFamily="Montserrat" color="white">TAKE THE QUIZ</Button>
+                </Link>
+            </Box>
+
         </Box>
     )
 }
