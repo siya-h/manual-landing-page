6b12087ee380837ef9eee2ce30fd33db2e1d81eb
Siyabonga Hlengwa <siyahlengwa@Siyas-Macbook-Pro.local>
2021-08-04 01:14:13 +0200

    styling laptop size body

--- src/components/ConditionItem.js
+++ src/components/ConditionItem.js
@@ -2,18 +2,21 @@ import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const ConditionItem = ({ item, containerStyle }) => {
-    const {imgSrc, heading, summary, description, number} = item
+    const { imgSrc, heading, summary, description, number } = item
     return (
         <Box style={containerStyle}>
-        <Text>{number}</Text>
-            <Box>
-                <Image src={imgSrc} />
+            <Text pos="absolute" fontSize="400px"  color="light_grey" zIndex="-1" right={['32%']}>{number}</Text>
+            <Box display="grid" gridTemplateColumns={['repeat(2, 1fr)']}>
+                <Box >
+                    <Image  src={imgSrc} />
+                </Box>
+                <Box w="70%" alignSelf="center">
+                    <Text color="tealish" textTransform="uppercase">{heading}</Text>
+                    <Text fontFamily="Helvetica">{summary}</Text>
+                    <Text fontFamily="Open Sans">{description}</Text>
+                </Box>
             </Box>
-            <Box>
-                <Text color="tealish" textTransform="uppercase">{heading}</Text>
-                <Text>{summary}</Text>
-                <Text>{description}</Text>
-            </Box>
+
 
         </Box>
     )
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -9,9 +9,9 @@ const LandingPage = () => {
     return (
         <Box>
             <Heading />
-            <Text >What we can help with</Text>
-            <ConditionItem containerStyle={{}} item={hairLoss}/>
-            <ConditionItem item={dysfunction} />
+            <Text fontFamily="Helvetica" textAlign="center" fontSize={['18px', '24px', '28px', '36px']} mt={['20px', '30px', '50px', '80px']} mb={['15px', '22.5px', '37.5px', '60px']}>What we can help with</Text>
+            <ConditionItem containerStyle={{ width: '65%', margin: ' 80px auto' }} item={hairLoss} />
+            <ConditionItem containerStyle={{ width: '65%', margin: '80px auto' }} item={dysfunction} />
             <Footer />
         </Box>
     )
