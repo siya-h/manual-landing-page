3957dcd565c185522221e8dd361e24ca28fef6b3
Siyabonga Hlengwa <siyahlengwa@Siyas-Macbook-Pro.local>
2021-08-03 23:15:19 +0200

    styling stanndard laptop view

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -10,7 +10,7 @@ const Heading = () => {
             bgSize="100% 100%"
             bgRepeat="no-repeat"
             w={['100%']}
-            h={['240px', '340px','460px' ,'680px']}
+            h={['240px', '340px','460px' ,'780px']}
             bgImage={bgImg}
             p={1}
         >
@@ -27,14 +27,14 @@ const Heading = () => {
                 </Link>
             </Box>
 
-            <Box fontSize={['22px', '36px','54px' ,'80px']} lineHeight={['22px', '42px', '62px','98px']} w={['55%', '55%', '60%','64%']} h={['15%', '20%','25%' ,'30%']} ml={['12px', '26px','34px', '60px']} mt={['15px', '25px','40px','90px']}>
+            <Box fontSize={['22px', '36px','54px' ,'80px']} lineHeight={['22px', '42px', '62px','98px']} w={['55%', '55%', '60%','64%']} h={['15%', '20%','25%' ,'27%']} ml={['12px', '26px','34px', '60px']} mt={['15px', '25px','40px','90px']}>
                 <Text transform="capitalize" >BE GOOD</Text>
                 <Text transform="capitalize" >TO YOURSELF</Text>
             </Box>
                 <Text fontFamily="Helvetica" fontSize={['12px', '14px','16px','18px']} lineHeight={['12px', '18px','24px','30px']} w={['55%', '40%','40%','400px']} h={['60px','65px','75px',]} ml={['14px','28px','38px','70px']} mt={['18px','26px',,]} >We’re working around the clock to bring you a holistic approach to your wellness. From top to bottom, inside and out.</Text>
             <Box ml={['14px', '28px', '38px','70px']} mt={['4px', '20px', '35px','50px']} display="inline-block" >
                 <Link to="/quiz">
-                    <Button bg="tealish" w={['100px', '160px','180px' ,'200px']} h={['38px', '40px','44px' ,'48px']} boxShadow={['0px 10px 30px -20px rgba(0,0,0,0.5)']} borderRadius={['49.5px']} fontSize={['10px', '12px', '12px', '12px']} letterSpacing={['1px',,,'2.4px']} maxWidth="200px" minWidth="150px" fontFamily="Montserrat" color="white">TAKE THE QUIZ</Button>
+                    <Button bg="tealish" w={['100px', '160px','180px' ,'200px']} _hover={{backgroundColor: 'pine_green'}} h={['38px', '40px','44px' ,'48px']} boxShadow={['0px 10px 30px -20px rgba(0,0,0,0.5)']} borderRadius={['49.5px']} fontSize={['10px', '12px', '12px', '12px']} letterSpacing={['1px',,,'2.4px']} maxWidth="200px" minWidth="150px" fontFamily="Montserrat" color="white">TAKE THE QUIZ</Button>
                 </Link>
             </Box>
 
